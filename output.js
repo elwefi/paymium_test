5bef8f97b713d3fe16018d2cb478005a2241a2ed
Pierre Michard <pierre.michard@paymium.com>
2018-03-23 18:51:29 +0100

    fix output

--- output.js
+++ output.js
@@ -1,12 +1,9 @@
 {
 	"users": [
 		{ "id": 1, "btc_balance": 1, "eur_balance": 5 },
-		{ "id": 2, "btc_balance": 5, "eur_balance": 1 }
+		{ "id": 2, "btc_balance": 9, "eur_balance": 5 }
 	],
-	"queued_orders": [
-		{ "id": 1, "user_id": 1, "direction": "buy", "btc_amount": 1, "price": 5 },
-		{ "id": 2, "user_id": 1, "direction": "sell", "btc_amount": 1, "price": 5 }
-	],
+	"queued_orders": [],
 	"orders": [
 		{ "id": 1, "user_id": 1, "direction": "buy", "btc_amount": 1, "price": 5, "state": "executed" },
 		{ "id": 2, "user_id": 1, "direction": "sell", "btc_amount": 1, "price": 5, "state": "executed" }
